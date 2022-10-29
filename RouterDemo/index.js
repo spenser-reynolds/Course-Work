9f88206005ef294ba7a2e7dda93ddf23a0df9735
Spenser Reynolds <spenser.reynolds@gmail.com>
2022-10-28 20:05:00 -0500

    Built basic express app using router to group routes.
    Added basic middleware for the admin pages.

--- RouterDemo/index.js
+++ RouterDemo/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const sheltersRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin');
+
+app.use('/shelters', sheltersRoutes);
+app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(3000, () => {
-    console.log('Serving app on localhost:3000')
+    console.log('Serving app on localhost:3000');
 })
