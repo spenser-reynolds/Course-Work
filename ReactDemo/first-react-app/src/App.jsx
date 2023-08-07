c2810a8e4784f5c25f799dceb6c9f60f3efc8d57
Spenser Reynolds <spenser.reynolds@gmail.com>
2023-08-07 12:33:34 -0500

    setup React Env

---
+++ ReactDemo/first-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Chicken from './Chicken';
+
+function App() {
+  return (
+    <div>
+      <Chicken />
+      <Chicken />
+      <Chicken />
+    </div>
+  )
+
+}
+
+export default App
